5ac7d44bfda334581bf2296ea7dda024fd52a19b
yan <yan.jingsheng@outlook.com>
2023-09-14 22:25:45 +0800

    script

--- script/new-kafka-topic.sh
+++ script/new-kafka-topic.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 /root/kafka/bin/zookeeper-server-start.sh /root/kafka/config/zookeeper.properties &
 /root/kafka/bin/kafka-server-start.sh /root/kafka/config/server.properties &
 /root/kafka/bin/kafka-topics.sh --create --zookeeper localhost:9092 --replication-factor 1 --partitions 1 --topic test &
--- script/submit.sh
+++ script/submit.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 find ../ -name "*.zip" -exec rm -r {} +
 zip -r -q "../src-$(date +%Y-%m-%d).zip" ../frontend/* ../src ../pom.xml ../demo.imi ../mvnw ../bookstore.imi -x '../frontend/node_modules/*'
