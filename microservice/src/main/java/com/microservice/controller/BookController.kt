7c8ae4f99c6888b5d91e3e091147186210ee5c82
ShadowNearby <yan.jingsheng@outlook.com>
2023-10-09 10:07:18 +0800

    microservice

--- microservice/src/main/java/com/microservice/controller/BookController.kt
+++ microservice/src/main/java/com/microservice/controller/BookController.kt
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/microservice")
 class BookController(
     private val bookService: BookService,
 ) {
